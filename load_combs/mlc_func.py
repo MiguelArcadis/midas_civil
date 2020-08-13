a1ac805234f9e4a08ca7f9e4c19a13fe76f7eb02
MiguelArcadis <69592091+MiguelArcadis@users.noreply.github.com>
2020-08-13 15:37:24 +0100

    uploaded files

---
+++ load_combs/mlc_func.py
@@ -0,0 +1,106 @@
+import numpy as np
+from  more_itertools import unique_everseen
+
+def zigzag(seq):
+    return seq[::2], seq[1::2]
+
+def get_combinations(curr_list, list1, list2, index, overall_list):
+    if index == len(list1) or index == len(list2):
+        overall_list.append(curr_list[:])
+        return
+
+    curr_list[index] = list1[index]
+    get_combinations(curr_list, list1, list2, index+1, overall_list)
+    curr_list[index] = list2[index]
+    get_combinations(curr_list, list1, list2, index+1, overall_list)
+
+def filter_permanent_groups(comb_permutations, list_of_comb_types):
+    permanent_group_indices = [index for index, comb_type in enumerate(list_of_comb_types) if comb_type == "PERM.g"] #Gets indices of PERMg loadcases
+    items_to_delete = []
+    
+    for permutation_index, permutation in enumerate(comb_permutations):
+        value_comparison = []
+        for index in permanent_group_indices:
+            value_comparison.append(permutation[index])
+
+        permanent_group_unfavourable = all(value >= 1 for value in value_comparison if value != 0)
+        permanent_group_favourable = all(value < 1 for value in value_comparison if value != 0)
+
+        if (permanent_group_unfavourable == False and permanent_group_favourable == False):
+            items_to_delete.append(permutation_index)
+
+    for item in sorted(items_to_delete, reverse = True):
+        del comb_permutations[item]
+
+    return comb_permutations
+
+def filter_main_variable_equal_0(comb_permutations, index_main_variable):
+    if index_main_variable == None:
+        return comb_permutations
+        
+    else:
+        items_to_delete = []
+        for i, permutation in enumerate(comb_permutations):
+            if permutation[index_main_variable] == 0:
+                items_to_delete.append(i)
+
+        for item in sorted(items_to_delete, reverse = True):
+            del comb_permutations[item]
+            
+        return comb_permutations
+
+def remove_duplicates(comb_permutations):
+    unique_comb_permutations = []
+    for comb in comb_permutations:
+        if comb not in unique_comb_permutations:
+            unique_comb_permutations.append(comb)
+    return unique_comb_permutations
+
+
+def df_to_mct(df):
+    full_array = np.array(df)
+
+    loadcases = np.array(df.columns.values)[1:]
+    combinations = full_array[2:, 0]
+    load_types = full_array[1, 1:]
+    factors = full_array[2:, 1:]
+
+    mct_string = '*LOADCOMB    ; Combinations\n; NAME=NAME, KIND, ACTIVE, bES, iTYPE, DESC, iSERV-TYPE, nLCOMTYPE, nSEISTYPE   ; line 1\n;      ANAL1, LCNAME1, FACT1, ...                                               ; from line 2'
+
+    #Creates load combinations
+    for row_index, row in enumerate(factors):
+        combination = combinations[row_index]
+        mct_string = mct_string + '\n' + '   NAME=' + str(combination) +', GEN, ACTIVE, 0, 0, , 0, 0, 0\n'
+
+        for factor_index, factor in enumerate(row):
+            load_type = load_types[factor_index]
+            loadcase = loadcases [factor_index]
+
+            if factor_index != 0:
+                mct_string = mct_string + ', '
+            mct_string = mct_string + str(load_type) + ', ' + str(loadcase) + ', ' + str(factor)
+
+    #Creates combination envelopes
+    combination_envelopes = []
+    for comb in combinations:
+        combination_envelopes.append(comb.split('_')[0])
+        combination_envelopes.append(comb.split('|')[0])
+
+    combination_envelopes = list(unique_everseen(combination_envelopes))
+ 
+    combination_groups = []
+    for envelope in combination_envelopes:
+        group = []
+        for comb in combinations:
+            if envelope in comb:
+                group.append(comb)
+        combination_groups.append(group)
+
+    for group_index, group in enumerate(combination_groups):
+        mct_string = mct_string + '\n' + '   NAME=' + str(combination_envelopes[group_index]) +', GEN, ACTIVE, 0, 1, , 0, 0, 0\n'
+        for comb_index, comb in enumerate(group):
+            if comb_index != 0:
+                mct_string = mct_string + ', '
+            mct_string = mct_string + 'CB' + ', ' + str(comb) + ', ' + '1.0'
+
+    return mct_string
---
+++ load_combs/mlc_main.py
@@ -0,0 +1,77 @@
+import pandas as pd
+import numpy as np
+import itertools
+import mlc_func
+pd.set_option('display.max_columns', 20)
+
+#Reads excel file and creats input dataframe
+file_path_in = r"C:\Users\silvam3530\ARCADIS\ASC HS2 Project Portal - 03 - Project Execution\07 - C3 Section\05 - Boddington Cutting\01 - BR - Bridges\Banbury Rd OB (Boddington)\02 Working\01 Structures\02 Models\Global\Combinations" + "\\"
+file_name_in = "BRB - Load Comb 3"
+extension_in = ".xlsx"
+
+file_path_out = file_path_in
+file_name_out = file_name_in + " _ MLC"
+extension_out = extension_in
+
+#Creates and processes input dataframe to rename columns and fill blanks
+xls_file = pd.ExcelFile(file_path_in + file_name_in + extension_in)
+df_in = pd.read_excel(xls_file, sheet_name = 'Combinations')
+df_in = df_in.fillna(method='ffill', axis=1)
+df_in.columns = pd.Series([np.nan if 'Unnamed:' in x else x for x in df_in.columns.values]).ffill().values.flatten()
+
+#Creates output dataframe with unique columns and clears all rows of content except first row that contains type of Loadcase for Midas
+df_out = pd.DataFrame()
+df_out = df_in.loc[:,~df_in.columns.duplicated()]
+df_out = df_out.iloc[:2]
+
+list_of_comb_types = df_in.iloc[0].values.tolist()[1:][::2]
+
+#Places all rows of the input dataframe in a list of lists.
+list_of_combs = df_in.iloc[2:].values.tolist()
+
+list_of_all_permutations = []
+
+for comb in list_of_combs:
+	comb_permutations = []
+	initial_comb_name = []
+	initial_comb_name.append(comb[0]) #Saves comb name in a list for later naming
+	
+	del comb[0] #Deletes comb name from the other list for data processing
+	index_main_variable = None
+
+	# Finds main variable and its index, removes the marking x and makes that values a float
+	for index, item in enumerate(comb):
+		if isinstance(item, str) == True:
+			index_main_variable = int(index / 2)
+			comb[index] = float(item[1:])
+
+	# Runs all the functions to create combinations and filter out unnecessary results
+
+	comb_upper, comb_lower = mlc_func.zigzag(comb)
+
+	current_list = [None] * min(len(comb_upper), len(comb_lower))
+	mlc_func.get_combinations(current_list, comb_upper, comb_lower, 0, comb_permutations)
+	mlc_func.filter_permanent_groups(comb_permutations, list_of_comb_types)
+	mlc_func.filter_main_variable_equal_0(comb_permutations, index_main_variable)
+	comb_permutations = mlc_func.remove_duplicates(comb_permutations)
+
+	# Adds a new column with the combination name and returns the new dataframe
+	final_comb_name = []
+	for i in range(len(comb_permutations)):
+	    final_comb_name.append(initial_comb_name[0] + '|' + str(i + 1))
+
+	for i, item in enumerate(comb_permutations):
+		item.insert(0, final_comb_name[i])
+	
+	list_of_all_permutations.extend(comb_permutations)
+
+
+df_out = df_out.append(pd.DataFrame(list_of_all_permutations, columns = df_out.columns))
+
+mct_string = mlc_func.df_to_mct(df_out)
+text_file = open(file_path_out + file_name_out + ".txt", "w")
+text_file.write(mct_string)
+text_file.close()
+
+
+df_out.to_excel(file_path_out + file_name_out + extension_out)
